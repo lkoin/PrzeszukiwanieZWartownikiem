c4be94103884efbc761718aafddd73adc1fcc29b
lkoin <lkoin.kontakt@gmail.com>
2023-09-27 08:28:25 +0200

    random number generation bugged

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 import static java.lang.Math.random;
 
 public class Main {
-    public static final int SIZE_ARRAY=50;
+    public static final int SIZE_ARRAY=15;
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbe ktora chcesz odszukac w zbiorze: ");
-        int lockedForNumber = scanner.nextInt();
+        for (int i=0;i<SIZE_ARRAY;i++) {
+            System.out.print(fillArray()[i]+ ", ");
+        }
+        System.out.println("\n Podaj liczbe ktora chcesz odszukac w zbiorze: ");
+        int lookedForNumber = scanner.nextInt();
 
+        findArray(lookedForNumber);
     }
 
     public static int[] fillArray() {
@@ -18,5 +22,29 @@ public class Main {
             arrayOfNumbers[i] = (int) (random() * 100);
         }
         return arrayOfNumbers;
+    }
+
+    public static void findArray(int lookedForNumber) {
+        boolean found = false;
+        for (int i = 0; i < SIZE_ARRAY; i++) {
+
+            if (fillArray()[i] == lookedForNumber && fillArray()[i] != SIZE_ARRAY - 1) {
+                found=true;
+                break;
+            }
+            else
+                found=false;
+
+
+        }
+        if (found==false) {
+            System.out.println("Znaleziono liczbe w zbiorze" + lookedForNumber);
+        }
+        else {
+            System.out.println("Nie znaleziono liczby w zbiorze");
+        }
+
+
+
     }
 }
