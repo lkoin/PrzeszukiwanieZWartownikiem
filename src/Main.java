db2990eceb87f4a1ddcd73b4edbcde8a7d1d3c11
lkoin <lkoin.kontakt@gmail.com>
2023-09-27 07:44:29 +0200

    first add

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int SIZE_ARRAY=50;
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe ktora chcesz odszukac w zbiorze: ");
+        int lockedForNumber = scanner.nextInt();
+
+    }
+
+    public static int[] fillArray() {
+        int[]arrayOfNumbers = new int[];
+    }
+}
